61ffbc11afe0e3ea960cc40da56ffb9de91b4467
LUX1 <LUX1@LUX-PC>
2016-11-24 20:20:44 +0100

    Début et test du text arial.ttf + Titillium-Light.ttf pour tester

--- 01_SFML_Snake/01_SFML_Snake/Main.cpp
+++ 01_SFML_Snake/01_SFML_Snake/Main.cpp
@@ -5,35 +5,65 @@
 // Création du projet
 // Date : 24/11/2016
 
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <stdio.h>
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "fonctions_snake.h"
+//#include <Text.hpp>
 
 using namespace std;
+string culoare;
 
-
+struct Game
+{
+	int pos[20][20];			// 0 - 19
+	char nom[20];
+}snake;
 
 //cout << "1" << endl;
 
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+	
+	sf::Font font;
+
+	if (!font.loadFromFile("Titillium-Light.ttf"))
+	{
+		// handle error
+	}
+
+	sf::Text text;
+	text.setFont(font);
+	text.setString("Bonjour");
+	text.setColor(sf::Color::White);
+	text.setCharacterSize(40);
+		
+
+	strcpy_s(snake.nom, "Lux");
 
 	while (window.isOpen())
 	{
+
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+		
+			if(event.type == sf::Keyboard::A)	
+				cout << "Nom : " << snake.nom << endl;
 		}
-		window.clear();
-		window.draw(shape);
-		window.display();
+
+
+			window.clear(sf::Color::Black);
+			window.draw(text);
+			window.display();
 		
 	}
 
