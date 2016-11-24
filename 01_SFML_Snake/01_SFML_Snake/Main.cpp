3e6ee7b24d92c15f980b00ad02dcf9f1c1f76a34
LUX1 <LUX1@LUX-PC>
2016-11-24 21:52:12 +0100

    Affichage F1 / F12 + lecture clavier

--- 01_SFML_Snake/01_SFML_Snake/Main.cpp
+++ 01_SFML_Snake/01_SFML_Snake/Main.cpp
@@ -51,7 +51,6 @@ struct Game
 // CODE PRINCIPAL
 //----------------------------------------------------
 
-// bonjour :D
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
@@ -68,13 +67,28 @@ int main()
 	
 	
 
-	sf::Text Menu;
+	sf::Text Menu, Text_start, Text_exit;
+	// MENU
 	Menu.setFont(font);
-	Menu.setString("!- Bienvenu dans le Snake -!");
+	Menu.setString(" Bienvenu dans le Snake ");
 	Menu.setColor(sf::Color::White);
 	Menu.setCharacterSize(40);
 	Menu.setOrigin(0, 0);
-	Menu.setPosition(150,0);
+	Menu.setPosition(150, 0);
+	// START
+	Text_start.setFont(font);
+	Text_start.setString("F1 - START");
+	Text_start.setColor(sf::Color::White);
+	Text_start.setCharacterSize(25);
+	Text_start.setOrigin(0, 0);
+	Text_start.setPosition(10, 400);
+	// EXIT
+	Text_exit.setFont(font);
+	Text_exit.setString("F12 - EXIT");
+	Text_exit.setColor(sf::Color::White);
+	Text_exit.setCharacterSize(25);
+	Text_exit.setOrigin(0, 0);
+	Text_exit.setPosition(10, 500);
 
 
 
@@ -87,17 +101,29 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		
-			if (event.type == sf::Event::KeyPressed)		
-				cout << "test" << endl;
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::F12))
+			{
+					window.close();
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::F1))
+			{
+				cout << "Bonjour :D " << endl;
+			}
+				
+			
+
 		}
 
 
 			window.clear(sf::Color::Black);
 			window.draw(Menu);
+			window.draw(Text_start);
+			window.draw(Text_exit);
 			window.display();
 		
 	}
 
 	cout << "EXIT" << endl;
+
 	return 0;
 }
